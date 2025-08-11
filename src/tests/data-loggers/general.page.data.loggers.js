2761b65dc5ea2e891eba1805ae54b4120085c2e7
RomanSibakovskij <benutzer373@gmail.com>
2025-08-11 18:37:18 +0300

    Add empty GeneralPageDataLoggers utility sheet

---
+++ src/tests/data-loggers/general.page.data.loggers.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const Logger = require("../../pages/utilities/logger.js")
+
+const BaseTest = require("../utilities/base.test");
+const {GeneralPage} = require("../../pages/general.page");
+
+class GeneralPageDataLoggers extends BaseTest {
+
+    constructor(driver) {
+        super();
+        this.driver = driver;
+    }
+
+    
+
+
+}
+module.exports = GeneralPageDataLoggers;
