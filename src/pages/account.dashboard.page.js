e1fb70ac60b2d7ab485900953e658be5677aeada
RomanSibakovskij <benutzer373@gmail.com>
2025-08-12 07:40:00 +0300

    Add empty AccountDashboardPage sheet

---
+++ src/pages/account.dashboard.page.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const {By} = require("selenium-webdriver");
+const BasePage = require("./utilities/base.page.js");
+const Logger = require("./utilities/logger");
+
+class AccountDashboardPage extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+
+
+}
+module.exports = AccountDashboardPage;
