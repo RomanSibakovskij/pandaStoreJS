7e4b368da4c95b52e835f953c0df877dec98e0e4
RomanSibakovskij <benutzer373@gmail.com>
2025-08-12 08:23:57 +0300

    Add empty PersonalInfoPageInvalidSingularInput utility sheet

---
+++ src/pages/personal-info-page-invalid-scenarios/personal.info.page.invalid.singular.input.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const {By} = require("selenium-webdriver");
+const BasePage = require("../utilities/base.page.js");
+const TestDataGenerator = require("../utilities/test.data.generator.js");
+const Logger = require("../utilities/logger");
+
+class PersonalInfoPageInvalidSingularInput extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+
+
+}
+module.exports = PersonalInfoPageInvalidSingularInput;
