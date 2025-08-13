1acd363e12a033f145168468f35521f8b4a22f43
RomanSibakovskij <benutzer373@gmail.com>
2025-08-13 04:26:51 +0300

    Add text element assert for AddressesDashPageTextElementAssert

--- src/tests/test-text-element-asserts/addresses.dash.page.text.element.assert.js
+++ src/tests/test-text-element-asserts/addresses.dash.page.text.element.assert.js
@@ -10,7 +10,39 @@ class AddressesDashPageTextElementAssert extends BaseTest{
         super(driver);
     }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    //addresses dashboard page text element assert test method
+    async isAddressesDashboardPageTextElementAsExpected(){
+        const addressesDashboardPage = new AddressesDashboardPage(this.driver);
+        //assert the addresses dashboard page title is as expected
+        const addressesDashPageTitle = await addressesDashboardPage.getAddressesDashboardPageTitle();
+        assert.strictEqual(addressesDashPageTitle, "YOUR ADDRESSES", "The addresses dashboard page title doesn't match expectations.");
+        //addresses box link elements
+        //assert the addresses dashboard page update link text is as expected
+
+        const addressesDashPageUpdateAddressLinkText = await addressesDashboardPage.getUpdateAddressLinkText();
+        addressesDashPageUpdateAddressLinkText.forEach((text, index) => {
+            assert.strictEqual(
+                text,
+                "Update",
+                `Update link text at index ${index} was "${text}", expected "Update"`
+            );
+        });
+        
+        //assert the addresses dashboard page delete link text is as expected
+        const addressesDashPageDeleteAddressLinkText = await addressesDashboardPage.getDeleteAddressLinkText();
+        addressesDashPageDeleteAddressLinkText.forEach((text, index) => {
+            assert.strictEqual(
+                text,
+                "Delete",
+                `Delete link text at index ${index} was "${text}", expected "Update"`
+            );
+        });
+    }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
 module.exports = AddressesDashPageTextElementAssert;
