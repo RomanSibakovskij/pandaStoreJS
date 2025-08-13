161fc386a38d862278de39ccfab01ddc5f00eeb4
RomanSibakovskij <benutzer373@gmail.com>
2025-08-13 04:30:08 +0300

    Add data logger for AddressesDashPageDataLogger

--- src/tests/data-loggers/addresses.dash.page.data.logger.js
+++ src/tests/data-loggers/addresses.dash.page.data.logger.js
@@ -11,6 +11,21 @@ class AddressesDashPageDataLogger extends BaseTest{
         this.driver = driver;
     }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //addresses dashboard page data logger method
+    async logAddressesDashPageData(){
+        const addressesDashboardPage = new AddressesDashboardPage(this.driver);
+        console.log("Displayed addresses dashboard page data: " + "\n")
+        //log addresses dashboard page title(s)
+        const addressesDashPageTitles = await addressesDashboardPage.getDisplayedUserAddressTitle();
+        Logger.info("Addresses dashboard page address title(s): " + addressesDashPageTitles);
+        //log addresses dashboard page address(es)
+        const addressesDashPageAddresses = await addressesDashboardPage.getDisplayedUserAddress();
+        Logger.info("Addresses dashboard page address address(es): " + addressesDashPageAddresses + "\n");
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
 module.exports = AddressesDashPageDataLogger;
