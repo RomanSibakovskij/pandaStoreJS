4740c4bc030e00b635b2efd0bfbf1282b5692e4e
RomanSibakovskij <benutzer373@gmail.com>
2025-08-13 04:25:14 +0300

    Add empty AddressesDashPageTextElementAssert utility sheet

---
+++ src/tests/test-text-element-asserts/addresses.dash.page.text.element.assert.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const assert = require("node:assert");
+const BaseTest = require("../utilities/base.test");
+const { AddressesDashboardPage } = require("../../pages/addresses.dashboard.page.js");
+
+class AddressesDashPageTextElementAssert extends BaseTest{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+
+}
+module.exports = AddressesDashPageTextElementAssert;
