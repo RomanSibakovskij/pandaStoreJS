1de46bf7a8a0bf1e137b3ad0a4c7e058ad5db7cf
RomanSibakovskij <benutzer373@gmail.com>
2025-08-14 06:03:27 +0300

    Add empty LoginRegisterDashPageInvalidSingularInput utility sheet

---
+++ src/pages/login-register-dash-page-invalid-scenarios/login.register.dash.page.invalid.singular.input.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const {By} = require("selenium-webdriver");
+const BasePage = require("../utilities/base.page.js");
+const Logger = require("../utilities/logger");
+
+class LoginRegisterDashPageInvalidSingularInput extends BasePage{
+
+    constructor(driver) {
+        super(driver);
+    }
+
+
+}
+module.exports = LoginRegisterDashPageInvalidSingularInput;
